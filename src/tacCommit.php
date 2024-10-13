a2c88d33c43ccf29ee9f862a3b31af7350a5d209
Keith Solomon <ksolomon@gmail.com>
2024-10-13 14:43:28 +0000

    ✨feature: Enhance commit message with timestamp and update navigation link

--- src/tacCommit.php
+++ src/tacCommit.php
@@ -3,7 +3,9 @@
 // $repoDir = '/data';
 $repoDir = '../config-test';
 $file = basename($_GET['file']);
-$commitMessage = "Updated " . $file;
+
+// Set the commit message
+$commitMessage = date('Y-m-d H:i:s')." - Updated ".$file;
 
 // Run Git commands to commit the changes
 exec("cd $repoDir && git add $file && git commit -m '$commitMessage'");
@@ -11,4 +13,4 @@ exec("cd $repoDir && git add $file && git commit -m '$commitMessage'");
 echo "Changes committed for $file.";
 ?>
 
-<a href="index.php">Back to file list</a>
+<a href="tacEdit.php?file=<?php echo urlencode($file); ?>">Back to editing</a>
